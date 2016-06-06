affbde8138861c1df66a919fd52e27a9c442d4d6
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-06-06 17:48:14 -0300

    adicionado filtros avançados de estado e municipio

--- Theme.php
+++ Theme.php
@@ -221,4 +221,66 @@ abstract class Theme extends BaseV1\Theme{
             }
         }
     }
+    
+    protected function _getAdvancedFilters(){
+        $estado = [
+                    'fieldType' => 'checklist',
+                    'isArray' => true,
+                    'label' => 'Estado',
+                    'placeholder' => 'Filtre por Estado',
+                    'filter' => [
+                        'param' => 'En_Estado',
+                        'value' => 'IN({val})'
+                    ],
+                    'parseValue' => ['join'],
+                    'options' => [
+                        ['value' => 'AC', 'label' => 'Acre'],
+                        ['value' => 'AL', 'label' => 'Alagoas'],
+                        ['value' => 'AP', 'label' => 'Amapá'],
+                        ['value' => 'AM', 'label' => 'Amazonas'],
+                        ['value' => 'BA', 'label' => 'Bahia'],
+                        ['value' => 'CE', 'label' => 'Ceará'],
+                        ['value' => 'DF', 'label' => 'Distrito Federal'],
+                        ['value' => 'ES', 'label' => 'Espírito Santo'],
+                        ['value' => 'GO', 'label' => 'Goiás'],
+                        ['value' => 'MA', 'label' => 'Maranhão'],
+                        ['value' => 'MT', 'label' => 'Mato Grosso'],
+                        ['value' => 'MS', 'label' => 'Mato Grosso do Sul'],
+                        ['value' => 'MG', 'label' => 'Minas Gerais'],
+                        ['value' => 'PA', 'label' => 'Pará'],
+                        ['value' => 'PB', 'label' => 'Paraíba'],
+                        ['value' => 'PR', 'label' => 'Paraná'],
+                        ['value' => 'PE', 'label' => 'Pernambuco'],
+                        ['value' => 'PI', 'label' => 'Piauí'],
+                        ['value' => 'RJ', 'label' => 'Rio de Janeiro'],
+                        ['value' => 'RN', 'label' => 'Rio Grande do Norte'],
+                        ['value' => 'RS', 'label' => 'Rio Grande do Sul'],
+                        ['value' => 'RO', 'label' => 'Rondônia'],
+                        ['value' => 'RR', 'label' => 'Roraima'],
+                        ['value' => 'SC', 'label' => 'Santa Catarina'],
+                        ['value' => 'SP', 'label' => 'São Paulo'],
+                        ['value' => 'SE', 'label' => 'Sergipe'],
+                        ['value' => 'TO', 'label' => 'Tocantins'],
+                    ],
+                ];
+        
+        $municipio = [
+                    'fieldType' => 'text',
+                    'label' => 'Município',
+                    'placeholder' => 'Busque por Município',
+                    'filter' => [
+                        'param' => 'En_Municipio',
+                        'value' => 'ILIKE(*{val}*)'
+                    ],
+                    'parseValue' => []
+                ];
+        
+        return [
+            'space' => [$estado, $municipio],
+            'agent' => [$estado, $municipio],
+            'event' => [],
+            'project' => []
+        ];
+    }
+
 }
